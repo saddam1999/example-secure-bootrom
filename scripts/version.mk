b9e9a9ac40543f6cf7e26a5ed9a20954ffe25a07
Yann GAUDE <yann.gaude@sifive.com>
2020-11-03 16:13:58 +0100

    Fix ticket SBL-115.

--- scripts/version.mk
+++ scripts/version.mk
@@ -4,7 +4,7 @@
 ## Current version of Secure Boot ROM
 __MAJOR_VERSION=0
 __MINOR_VERSION=0
-__EDIT_VERSION=2
+__EDIT_VERSION=1
 
 ## Base reference version of Secure Boot ROM
 __BREF_MAJOR_VERSION=0
